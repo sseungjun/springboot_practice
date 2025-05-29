4c8470bf4089c0822f282e95e15678cd4eaf13fe
sseungjun <v0501b@daum.net>
2025-05-30 00:23:04 +0900

    초기 커밋

---
+++ src/main/java/com/core/DDuBBukcho/DDuBBukchoApplication.java
@@ -0,0 +1,13 @@
+package com.core.DDuBBukcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDuBBukchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDuBBukchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/DDuBBukcho/DDuBBukchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.DDuBBukcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDuBBukchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
