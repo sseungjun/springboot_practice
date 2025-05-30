cff63057c4054977d78d967f310341397dc9e4a9
sseungjun <v0501b@daum.net>
2025-05-31 03:00:21 +0900

    feat: hello API 추가 및 DB 코드 임시 제거4

--- src/main/java/com/core/DDuBBukcho/DDuBBukchoApplication.java
+++ src/main/java/com/core/DDuBBukcho/DDuBBukchoApplication.java
@@ -2,8 +2,9 @@ package com.core.DDuBBukcho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DDuBBukchoApplication {
 
 	public static void main(String[] args) {
